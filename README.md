c9e461f784310bba63aee600ee5df54bb13cd6fc
iamebuka <obukweluebuka@gmail.com>
2017-03-05 22:07:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RTBot
+A facebook messenger chat bot that estimate traffic time and suggest alternative root to its connections
---
+++ index.js
@@ -0,0 +1,18 @@
+﻿require('dotenv').config();
+
+var express = require("express");
+
+var googleMapsClient = require('@google/maps').createClient({
+ key: process.env.Google_API_Key
+});
+
+
+googleMapsClient.geocode({
+address:"1600 amphitheatre parkway mountain view california"
+},
+function(err, response){
+if(!err){
+ console.log(response.json.results[0].geometry.location.lat);
+} })
+
+
