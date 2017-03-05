087e9f5e2f787198b64af992b473fa5719d989ee
iamebuka <obukweluebuka@gmail.com>
2017-03-06 00:41:50 +0100

    minor update

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 ﻿require('dotenv').config();
 
 var express = require("express");
+var distance = require('google-distance-matrix');
 
-var googleMapsClient = require('@google/maps').createClient({
- key: process.env.Google_API_Key
-});
+distance.key('AIzaSyD3zxw5htVy5M3RKK_weRECSM3rkfVtxZ8');
+distance.departure_time(new Date().getUTCDate);
+distance.mode('driving');
 
+var origins = ['yaba lagos'];
+var destinations = ['lekki lagos'];
 
-googleMapsClient.geocode({
-address:"1600 amphitheatre parkway mountain view california"
-},
-function(err, response){
-if(!err){
- console.log(response.json.results[0].geometry.location.lat);
-} })
+distance.matrix(origins, destinations, function (err, distances) {
+   if(distances) {
+      console.log(distances);
+    }
+    if (!err){
+      for (var i=0; i < origins.length; i++) {
+            for (var j = 0; j < destinations.length; j++) {
+            if (distances.rows[0].elements[j].status == 'OK') {
+                    var distance = distances.rows[i].elements[j];
+                    console.log('Distance from ' + origins + ' to ' + destinations + ' is ' + JSON.stringify(distance));
+                } else {
+                    console.log(destinations + ' is not reachable by land from ' + origins);
+                }
+          
+        }}
+           
 
-
+    }
+})
